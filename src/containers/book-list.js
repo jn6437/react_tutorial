2bf9cf6fd5ccd5855c53fd9cd39ef5ec5d5a6f43
jn6437 <jn6437@gmail.com>
2018-08-13 10:59:15 +1000

    simple redux integration with readme update

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
+//react-redux is the glue between react and redux
 import { connect } from "react-redux";
 
-export default class BookList extends Component {
+class BookList extends Component {
   renderList() {
     return this.props.books.map(book => {
       return (
@@ -12,6 +13,21 @@ export default class BookList extends Component {
     });
   }
   render() {
+    //console.log(this.props.asdf); // -> '123'
     return <ul className="list-group col-sm-4">{this.renderList()}</ul>;
   }
 }
+
+//whatever object retuned here will be available in component by this.props
+//if the state changes this function will re-render
+function mapStateToProps(state) {
+  //whatever is returned will show up as props inside of booklist
+  return {
+    //asdf: "123"
+    books: state.books
+  };
+}
+
+//connect takes function and component and produces a container
+//container is a component that is aware of the state that is contained by redux
+export default connect(mapStateToProps)(BookList);
