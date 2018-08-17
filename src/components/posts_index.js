faffe1c2f7b2dbcaff9ccd5db264bde4d836777f
jn6437 <jn6437@gmail.com>
2018-08-17 10:43:21 +1000

    Router and link setup 131

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,8 +1,46 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { fetchPosts } from "../actions";
+import _ from "lodash";
 
 class PostsIndex extends Component {
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+
+  renderPosts() {
+    //cannot use this.props.posts.map on object
+    return _.map(this.props.posts, post => {
+      return (
+        <li className="list-group-item" key={post.id}>
+          {post.title}
+        </li>
+      );
+    });
+  }
+
   render() {
-    return <div>post PostsIndex</div>;
+    return (
+      <div>
+        <div className="text-xs-right">
+          {/*Link tag is <a> tag, but it prevents from <a>'s normal behaviour*/}
+          <Link className="btn btn-primary" to="/posts/new">
+            Add a post
+          </Link>
+        </div>
+
+        <h3>posts</h3>
+        <ul className="list-group">{this.renderPosts()}</ul>
+      </div>
+    );
   }
 }
-export default PostsIndex;
+
+function mapStateToProps(state) {
+  return { posts: state.posts };
+}
+export default connect(
+  mapStateToProps,
+  { fetchPosts } //{ fetchPosts: fetchPosts}
+)(PostsIndex);
